98a8e700e7d30e60ad4245e2c953496781273b46
tsedey <167250386+tsedeysolomons@users.noreply.github.com>
2026-01-05 23:28:04 +0300

    feat(data): configure AppDbContext for User entity
    - Add Users DbSet to AppDbContext
    - Configure unique index for User email
    - Set default SQL value (GETDATE()) for CreatedAt property

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5220eb000976dab3e41cc802c14072bda9064d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282c35422a4bf2a1b495ed041bb821fee7964860")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
